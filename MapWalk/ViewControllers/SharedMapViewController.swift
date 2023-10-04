a92765411663fbe26170b5ef2620be9942e232c5
Bhautik Patoliya <bhautikpatoliyalive@gmail.com>
2023-10-04 18:21:53 +0530

    #24 Menu Edits #21 Line/Area toggle

--- MapWalk/ViewControllers/SharedMapViewController.swift
+++ MapWalk/ViewControllers/SharedMapViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol SharedMapDelegate: AnyObject {
     func showSelectedMapFromURL(url: URL)
+    func showCurrentMap()
 }
 
 class SharedMapViewController: UIViewController {
@@ -16,6 +17,9 @@ class SharedMapViewController: UIViewController {
     weak var delegate: SharedMapDelegate?
     @IBOutlet weak var tblView: UITableView!
     @IBOutlet weak var viewContainer: UIView!
+    @IBOutlet weak var lblNoContent: UILabel!
+    var isOpenedMapDeleted = false
+    var openedMapURL: URL?
     
     var arrKMLFiles: [URL] = []
     
@@ -26,14 +30,37 @@ class SharedMapViewController: UIViewController {
     }
     
     func setupView() {
-        self.getKMLFiles()
         self.tblView.delegate = self
         self.tblView.dataSource = self
         self.tblView.register(UINib(nibName: "SharedMapTableViewCell", bundle: nil), forCellReuseIdentifier: "SharedMapTableViewCell")
         
+        self.getKMLFiles()
         // corner radius
         viewContainer.layer.masksToBounds = true
         viewContainer.layer.cornerRadius = 10
+        
+        self.lblNoContent.isHidden = true
+    }
+    
+    func showContentUnavailableView() {
+        if #available(iOS 17.0, *) {
+            self.lblNoContent.isHidden = true
+            var config = UIContentUnavailableConfiguration.empty()
+            config.image = UIImage(systemName: "map.fill")
+            config.text = "No Shared Maps"
+            config.secondaryText = "Your shared maps will appear here."
+            config.imageProperties.tintColor = .gray
+            config.textProperties.color = .gray
+            config.secondaryTextProperties.color = .gray
+            self.contentUnavailableConfiguration = config
+        }
+        else {
+            // Fallback on earlier versions
+            DispatchQueue.main.async {
+                self.tblView.isHidden = true
+                self.lblNoContent.isHidden = false
+            }
+        }
     }
 
     func getKMLFiles() {
@@ -44,7 +71,11 @@ class SharedMapViewController: UIViewController {
                 for itemUrl in directoryContents {
                     self.arrKMLFiles.append(itemUrl)
                 }
+                
                 self.tblView.reloadData()
+                if self.arrKMLFiles.count == 0 {
+                    self.showContentUnavailableView()
+                }
             }
         }
         catch {
@@ -76,5 +107,62 @@ extension SharedMapViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.delegate?.showSelectedMapFromURL(url: self.arrKMLFiles[indexPath.row])
         self.dismiss(animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        // Swipe-to-delete action
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+            self.showDeleteConfirmation(indexPath: indexPath)
+            completionHandler(true)
+        }
+        
+        deleteAction.backgroundColor = .red
+        //renameAction.backgroundColor = .blue
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
+    }
+    
+    @objc func showDeleteConfirmation(indexPath: IndexPath) {
+        let alertController = UIAlertController(
+            title: "Delete Map",
+            message: "Are you sure you want to delete?",
+            preferredStyle: .actionSheet
+        )
+        
+        let mapURL = self.arrKMLFiles[indexPath.row]
+        let deleteAction = UIAlertAction(title: "Delete map", style: .destructive) { _ in
+            // Performing the delete action
+            
+            if let directoryURL = Utility.getDirectoryPath(folderName: DirectoryName.ImportedKMLFile) {
+                let destinationURL = directoryURL.appendingPathComponent(mapURL.lastPathComponent)
+                if FileManager.default.fileExists(atPath: destinationURL.path) {
+                    try? FileManager.default.removeItem(at: destinationURL)
+                    self.arrKMLFiles.remove(at: indexPath.row)
+                    self.tblView.deleteRows(at: [indexPath], with: .automatic)
+                    if self.openedMapURL != nil && self.openedMapURL == mapURL {
+                        self.delegate?.showCurrentMap()
+                    }
+                }
+                if self.arrKMLFiles.count == 0 {
+                    self.showContentUnavailableView()
+                }
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        
+        if let popoverController = alertController.popoverPresentationController {
+            // Set the source view for iPad and other devices with popover support
+            popoverController.sourceView = view
+            popoverController.sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
